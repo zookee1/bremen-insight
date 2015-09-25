27623ef09dc85ee19143bee97faec07b865bf591
Artur Hallmann <a.hallmann@neusta.de>
2015-09-25 23:47:16 +0200

    bremen

--- Source/src/BremenHack/InsightBundle/Resources/public/js/us-states.js
+++ Source/src/BremenHack/InsightBundle/Resources/public/js/us-states.js
@@ -1,4 +1,4 @@
-{
+var statesData = {
   "type": "FeatureCollection",
   "generator": "overpass-turbo",
   "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
@@ -29451,4 +29451,4 @@
       }
     }
   ]
-}
+};
