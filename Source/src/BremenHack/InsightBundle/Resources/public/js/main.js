8c105167eb8ebb14c04642bda47d789a164df6a1
Nick <hi@nkls.cc>
2015-09-27 11:50:45 +0200

    started refactoring

---
+++ Source/src/BremenHack/InsightBundle/Resources/public/js/main.js
@@ -0,0 +1,67 @@
+function loadDataset(id, level) {
+    selectedDataset = id;
+    selectedLevel = level;
+    $.ajax({
+        url: '/geojson/' + selectedDataset + '/' + selectedLevel,
+        data: 'json',
+        error: function(err){
+            console.log(err);
+            alert("fail!");
+        },
+        beforeSend: function(){
+            console.log('started');
+        },
+        done: function(data){
+            console.log(1);
+            currentData = data;
+            selectedCategory = data.categories[0];
+            refreshDataPoints();
+            $('#categorySelect option').remove();
+            $('#datasetSelect option').remove();
+            $('#columnSelect option').remove();
+
+            for (var i=0; i<data.categories.length; i++) {
+                var category = data.categories[i];
+                $('#categorySelect').append('<option value="' + category + '"' + (category === selectedCategory ? ' selected' : '') + '>' + category + '</option>');
+            }
+            var columnOptions = [];
+            for(var i=0; i<data.columns.length; i++) {
+                var column = data.columns[i];
+                $('#columnSelect').append('<option value="' + i + '"' + (i === selectedColumn ? ' selected' : '') + '>' + column + '</option>');
+            }
+            var datasetOptions = [];
+            for(var i=0; i<data.datasets.length; i++) {
+                var dataset = data.datasets[i];
+                $('#datasetSelect').append('<option value="' + dataset.id + '"' + (selectedDataset == dataset.id ? ' selected' : '') + '>' + dataset.label + '</option>');
+            }
+            $('#categorySelect').change(function() {
+                selectedCategory = $('#categorySelect').val();
+                refreshDataPoints();
+            });
+            $('#columnSelect').change(function() {
+                var val = $('#columnSelect').val();
+                selectedColumn = val;
+                refreshDataPoints();
+            });
+            $('#datasetSelect').change(function() {
+                var val = $('#datasetSelect').val();
+                loadDataset(val, selectedLevel);
+            });
+            $('#bar').show();
+        },
+        success: function(){
+            console.log('success');
+        }
+    });
+}
+
+function levelSwitcher(){
+    $('#levelSwitcher').click(function() {
+        loadDataset(selectedDataset, selectedLevel == 10 ? 11 : 10);
+    });
+}
+
+$(document).ready(function(){
+    loadDataset('17397', 10);
+    levelSwitcher();
+});
---
+++ Source/src/BremenHack/InsightBundle/Resources/public/js/map.js
@@ -0,0 +1,145 @@
+var map = L.map('map').setView([53.083333, 8.8], 10);
+var selectedCategory, selectedColumn = 0, selectedDataset, selectedLevel, currentData, legend, controls;
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a> ' +
+    'Data &copy; <a href="http://daten.bremen.de/">Bremen</a>',
+    id: 'mapbox.light'
+}).addTo(map);
+
+
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Bremen Insight Test</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + extractValue(props) + ' ' + currentData.columns[selectedColumn]
+            : 'Hover over a Stadtteil');
+};
+
+info.addTo(map);
+
+
+// get color depending on population density value
+function getColor(d) {
+    var grades = getScala();
+    return d > grades[7] ? '#800026' :
+        d > grades[6]  ? '#BD0026' :
+            d > grades[5]  ? '#E31A1C' :
+                d > grades[4]  ? '#FC4E2A' :
+                    d > grades[3]   ? '#FD8D3C' :
+                        d > grades[2]   ? '#FEB24C' :
+                            d > grades[1]   ? '#FED976' :
+                                '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 1,
+        opacity: 1,
+        color: '#fff',
+        dashArray: '',
+        fillOpacity: 0.8,
+        fillColor: getColor(extractValue(feature.properties))
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 3,
+        color: '#fff',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+function extractValue(props) {
+    if(!props || !props.dataPoints || !props.dataPoints[selectedCategory]) {
+        return null;
+    }
+    return props.dataPoints[selectedCategory][selectedColumn];
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+function getScala() {
+    var maxPrecision = currentData.columnMaxima[selectedColumn] > 1000 ? 1000 : 100;
+    var maxima = Math.ceil(currentData.columnMaxima[selectedColumn] / maxPrecision) * maxPrecision;
+    var minPrecision = currentData.columnMinima[selectedColumn] > 1000 ? 1000 : 100;
+    var minima = Math.ceil(currentData.columnMinima[selectedColumn] / minPrecision) * minPrecision;
+    var grades = [];
+    for(var i=0; i<8; i++) {
+        grades.push(minima + Math.floor((maxima-minima) / 8 * i));
+    }
+    return grades;
+}function refreshDataPoints() {
+    if(geojson) {
+        //geojson.removeFrom(map);
+    }
+    geojson = L.geoJson(currentData, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+    var grades = getScala();
+
+    if(legend) {
+        legend.removeFrom(map);
+    }
+
+
+    legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            labels = [],
+            from, to;
+
+        for (var i = 0; i < grades.length; i++) {
+            from = grades[i];
+            to = grades[i + 1];
+
+            labels.push(
+                '<i style="background:' + getColor(from + 1) + '"></i> ' +
+                from + (to ? '&ndash;' + to : '+'));
+        }
+
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    legend.addTo(map);
+}
